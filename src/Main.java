993d40b278801fabc4bd0c9390aded0e225b9600
vrnkdm <vdemcenko234@gmail.com>
2026-02-11 08:40:11 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-//        Mainwindow nk0 = new Mainwindow();
-//        Author nk1 = new Author();
-//        Course nk2 = new Course();
-//        Test nk3 = new Test();
-//        Encryption nk4 = new Encryption();
        SwingUtilities.invokeLater(Mainwindow::new);
     }
 }
